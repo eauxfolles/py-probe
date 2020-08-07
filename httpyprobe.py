89b17915e7bbd564c666ee8563b52db6c392314f
Andreas Tokarczyk <andreas_tokarczyk@web.de>
2020-08-07 23:13:53 +0200

    Update httpyprobe.py

--- httpyprobe.py
+++ httpyprobe.py
@@ -1,1 +1,11 @@
-WIP
+#!/usr/bin/env python3
+# ==============================================================================
+# Name: httpyprobe.py (HTTP Python Probe)
+# Version: v1 (alpha)
+# Author: eauxfolles
+# Date: 07.08.2020
+# Description: Script to check if HTTP and HTTPS domains are resolving. 
+# Usage: "httpyprobe.py <list> -t <number>"
+# ==============================================================================
+
+# WIP
