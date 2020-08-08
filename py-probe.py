c0d1f734aced0aace35cedd85f80199eb0197cc2
Andreas Tokarczyk <andreas_tokarczyk@web.de>
2020-08-08 13:46:38 +0200

    Update and rename httpyprobe.py to py-probe.py

--- py-probe.py
+++ py-probe.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # ==============================================================================
-# Name: httpyprobe.py (HTTP Python Probe)
-# Version: v1 (alpha)
+# Name: pyprobe.py (Python Probe)
+# Version: v2 (alpha)
 # Author: eauxfolles
-# Date: 07.08.2020
+# Date: 08.08.2020
 # Description: Script to check if HTTP and HTTPS domains are resolving. 
-# Usage: "httpyprobe.py <list> -t <number>"
+# Usage: "pyprobe.py <list> -t <number>"
 # ==============================================================================
 
 # WIP
