a45029c6aa38da46b943a15d2c27edc0f940ac11
Andreas Tokarczyk <andreas_tokarczyk@web.de>
2020-08-11 22:52:04 +0200

    Update pyprobe.py

--- pyprobe.py
+++ pyprobe.py
@@ -8,4 +8,73 @@
 # Usage: "probe.py <source> <target>"
 # ==============================================================================
 
-# WIP
+import sys
+import socket
+import requests
+
+source_file = False
+target_file = False
+source_list = []
+process_list = []
+
+# function to validate input provided with command line (has to be 1 or 3 parameters or -help/--help)
+def validate_command_line():
+
+	source_file = False
+
+	# validate correct use of calling an options or parameter and set variable
+	if len(sys.argv) < 2:
+		print("error: no parameters provided")
+		exit()
+	elif len(sys.argv) > 3:
+		print("error: too many parameters provided")
+		exit()
+	elif sys.argv[1] == "-help" or sys.argv[1] == "--help":
+		print("usage: httpyprobe.py <source> <target>")
+		exit()
+	elif len(sys.argv) == 3:
+		source_file = sys.argv[1]
+		target_file = sys.argv[2]
+	else:
+		print("error: please check correct usage of parameters")
+		exit()
+
+	return source_file, target_file
+
+# function to load entries from source-file into variable
+def load_list():
+
+	with open(source_file) as f: 
+		source_list = [line.rstrip() for line in f.readlines()]
+
+	return source_list
+
+# function to probe for http (port 80) and https (port 443) of each entry
+def probe(single_entry, protocol): 
+
+	single_entry_url = protocol + single_entry
+
+	try:
+		response = requests.get(single_entry_url, timeout=1, allow_redirects=False)
+		if response.status_code == 200: 
+			print(response.url + " resolved")
+		elif (response.status_code == 301) or (response.status_code == 302):
+			print(response.url + " redirected to " + str(response.headers['Location']))
+		else: 
+			print(response.url + " caused " + response.status_code)
+	except requests.Timeout:
+		print(single_entry_url + " request timed out")
+		pass
+	except requests.ConnectionError:
+		print(single_entry_url + " url does not resolve")
+		pass
+	except:
+		pass
+
+
+source_file, target_file = validate_command_line()
+source_list = load_list()
+
+for i in source_list:
+	probe(i, "http://")
+	probe(i, "https://")
