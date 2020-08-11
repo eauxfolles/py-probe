2388034c80df862fd4110881ce6ed7389c23fcc0
Andreas Tokarczyk <andreas_tokarczyk@web.de>
2020-08-11 22:42:36 +0200

    Update pyprobe.py

--- pyprobe.py
+++ pyprobe.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # ==============================================================================
 # Name: pyprobe.py (Python Probe)
-# Version: v2 (alpha)
+# Version: v3 (alpha)
 # Author: eauxfolles
-# Date: 08.08.2020
+# Date: 11.08.2020
 # Description: Script to check if HTTP and HTTPS domains are resolving. 
-# Usage: "pyprobe.py <list> -t <number>"
+# Usage: "probe.py <source> <target>"
 # ==============================================================================
 
 # WIP
